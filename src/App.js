5c13049a49169fa9a144560d4cf5eb7812973cb2
Pascal Bürklin <pascal@buerkl.in>
2023-12-05 14:48:46 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import SimpleTable from './Components/SimpleTable';
+import {AppBar, Button, Toolbar, Typography} from "@mui/material"; // Adjust the path as needed
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <h1></h1>
+          <AppBar position="static" style={{padding:'0px'}}>
+              <Toolbar>
+                  <Typography variant="h6" style={{ flexGrow: 1 ,padding:'0px'}}>
+                      Yelp Reviews
+                  </Typography>
+                  <Button color="inherit">Table</Button>
+                  <Button color="inherit">Add Review</Button>
+              </Toolbar>
+          </AppBar>
+        <SimpleTable />
+      </div>
   );
 }
 
